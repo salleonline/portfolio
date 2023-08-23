9ecb514573d56d7b1a116537752cee04bf8c8251
Safo Emmanuel <nanasafo122@gmail.com>
2023-08-23 03:13:04 +0000

    second commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,13 +15,13 @@ const AboutPage = () => {
         </p>
         <div className="mt-10 max-w-2xl space-y-6 text-base">
           <p>
-            Studio was started by three friends who noticed that developer
+            SALLE was started by Two friends who noticed that developer
             studios were charging clients double what an in-house team would
             cost. Since the beginning, we have been committed to doing things
-            differently by charging triple instead.
+            differently by charging less instead.
           </p>
           <p>
-            At Studio, we’re more than just colleagues — we’re a family. This
+            At SALLE, we’re more than just colleagues — we’re a family. This
             means we pay very little and expect people to work late. We want our
             employees to bring their whole selves to work. In return, we just
             ask that they keep themselves there until at least 6:30pm.
